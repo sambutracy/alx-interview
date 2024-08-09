4e94ea698a1407dde47c7dbb7c0bd2dd34c56c43
sambutracy <tracysambut@gmail.com>
2024-08-09 03:39:48 +0300

    fix errors pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 def minOperations(n):
-  if n <= 1:
-    return 0
+    """Calculate the minimum number of operations needed
+    to achieve n 'H' characters"""
+    if n <= 1:
+        return 0
 
-  operations = 0
-  divisor = 2
+    operations = 0
+    divisor = 2
 
-  while n > 1:
-    while n % divisor == 0:
-      operations += divisor
-      n //= divisor
-    divisor += 1
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
 
-  return operations
+    return operations
