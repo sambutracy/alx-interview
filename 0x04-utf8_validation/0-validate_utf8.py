96a63f8bf14a840862bff8087f580eccefbe6151
sambutracy <tracysambut@gmail.com>
2024-08-22 23:10:25 +0300

    fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Determines if a given data set represents a valid UTF-8 encoding.
+"""
+
+
 def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
