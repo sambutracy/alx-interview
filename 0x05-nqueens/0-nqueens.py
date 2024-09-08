ef1c06ba918cfb948c3657e0705255237c1d80d1
sambutracy <tracysambut@gmail.com>
2024-09-08 16:31:29 +0300

    add task 0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,45 +1,46 @@
 #!/usr/bin/node
 
-// Import the request module to make HTTP requests
 const request = require('request');
 
-// Function to print characters in the correct order using recursion
-function order(characters, idx) {
+// Function to print character names in order
+function fetchCharacters(characters, idx) {
   if (idx >= characters.length) {
-    return; // Base case: if index is out of bounds, stop recursion
+    return; // Base case: if the index is out of bounds, stop recursion
   }
-  
+
   // Request the character details from the character URL
-  request(characters[idx], function (err, response, body) {
+  request(characters[idx], (err, response, body) => {
     if (err) {
-      console.log(err);
+      console.error(err);
     } else if (response.statusCode === 200) {
-      const person = JSON.parse(body); // Parse the character data
-      console.log(person.name); // Print the character's name
-      order(characters, idx + 1); // Recurse for the next character
+      const character = JSON.parse(body);
+      console.log(character.name); // Print the character's name
+      fetchCharacters(characters, idx + 1); // Recurse for the next character
     } else {
-      console.log('Error occurred, Status code: ' + response.statusCode);
+      console.error('Failed to retrieve character, status code:', response.statusCode);
     }
   });
 }
 
-// Main logic
-const ep = process.argv[2]; // Get the movie ID from the command-line argument
-if (!ep) {
-  console.log('Usage: ./0-starwars_characters.js <Movie ID>');
+// Get the Movie ID from the command-line argument
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
   process.exit(1);
 }
 
-const url = 'https://swapi-api.hbtn.io/api/films/';
+// Construct the URL for the Star Wars API
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-// Make the initial request to get the movie data
-request(url + ep, function (err, response, body) {
+// Fetch the movie data based on the Movie ID
+request(url, (err, response, body) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else if (response.statusCode === 200) {
     const movieData = JSON.parse(body); // Parse the movie data
-    order(movieData.characters, 0); // Call the function to print characters in order
+    const characters = movieData.characters; // Get the list of character URLs
+    fetchCharacters(characters, 0); // Start fetching characters in order
   } else {
-    console.log('Error occurred, Status code: ' + response.statusCode);
+    console.error('Failed to retrieve movie, status code:', response.statusCode);
   }
 });
