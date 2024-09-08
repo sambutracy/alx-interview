ac44539fad9753e0c09a70fe4145507f30dc257d
sambutracy <tracysambut@gmail.com>
2024-09-08 16:24:15 +0300

    fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,63 +1,45 @@
-#!/usr/bin/python3
-"""
-Module for 0x0C. N Queens.
-Specializations - Interview Preparation ― Algorithms
-"""
-from sys import argv, exit
+#!/usr/bin/node
 
+// Import the request module to make HTTP requests
+const request = require('request');
 
-def solveNQueens(n):
-    """Program that solves the N queens problem"""
-    res = []
-    queens = [-1] * n
+// Function to print characters in the correct order using recursion
+function order(characters, idx) {
+  if (idx >= characters.length) {
+    return; // Base case: if index is out of bounds, stop recursion
+  }
+  
+  // Request the character details from the character URL
+  request(characters[idx], function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      const person = JSON.parse(body); // Parse the character data
+      console.log(person.name); // Print the character's name
+      order(characters, idx + 1); // Recurse for the next character
+    } else {
+      console.log('Error occurred, Status code: ' + response.statusCode);
+    }
+  });
+}
 
-    def dfs(index):
-        """Recursively resolves the N queens problem"""
-        if index == len(queens):  # n queens]placed correctly
-            res.append(queens[:])
-            return
-        for i in range(len(queens)):
-            queens[index] = i
-            if valid(index):
-                dfs(index + 1)
+// Main logic
+const ep = process.argv[2]; // Get the movie ID from the command-line argument
+if (!ep) {
+  console.log('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
 
-    def valid(n):
-        """Check whether nth queen can be placed"""
-        for i in range(n):
-            if abs(queens[i] - queens[n]) == n - i:  # same diagonal
-                return False
-            if queens[i] == queens[n]:
-                return False
-        return True
+const url = 'https://swapi-api.hbtn.io/api/films/';
 
-    def make_all_boards(solutions):
-        """Build the list that will be returned"""
-        actual_boards = []
-        for queens_solution in solutions:
-            board = []
-            for row, col in enumerate(queens_solution):
-                board.append([row, col])
-            actual_boards.append(board)
-        return actual_boards
-
-    dfs(0)
-    return make_all_boards(res)  # Ensure this return is correctly indented
-
-
-if __name__ == "__main__":
-    if len(argv) < 2:
-        print('Usage: nqueens N')
-        exit(1)
-    try:
-        n = int(argv[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
-
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
-    else:
-        result = solveNQueens(n)
-        for row in result:
-            print(row)
+// Make the initial request to get the movie data
+request(url + ep, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const movieData = JSON.parse(body); // Parse the movie data
+    order(movieData.characters, 0); // Call the function to print characters in order
+  } else {
+    console.log('Error occurred, Status code: ' + response.statusCode);
+  }
+});
