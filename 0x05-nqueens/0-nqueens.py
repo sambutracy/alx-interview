27ede7e813f108954564af1c9c1ffea05238007e
sambutracy <tracysambut@gmail.com>
2024-09-08 16:43:08 +0300

    add

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,46 +1,67 @@
-#!/usr/bin/node
+#!/usr/bin/python3
+"""This module solves the N queens problem
+"""
+import sys
 
-const request = require('request');
 
-// Function to print character names in order
-function fetchCharacters(characters, idx) {
-  if (idx >= characters.length) {
-    return; // Base case: if the index is out of bounds, stop recursion
-  }
+if (len(sys.argv) != 2):
+    print("Usage: nqueens N")
+    exit(1)
+try:
+    N = int(sys.argv[1])
+except Exception:
+    print("N must be a number")
+    exit(1)
+if (N < 4):
+    print("N must be at least 4")
+    exit(1)
 
-  // Request the character details from the character URL
-  request(characters[idx], (err, response, body) => {
-    if (err) {
-      console.error(err);
-    } else if (response.statusCode === 200) {
-      const character = JSON.parse(body);
-      console.log(character.name); // Print the character's name
-      fetchCharacters(characters, idx + 1); // Recurse for the next character
-    } else {
-      console.error('Failed to retrieve character, status code:', response.statusCode);
-    }
-  });
-}
+# create board (list of lists to hold queens)
+board = [[0 for i in range(N)] for i in range(N)]
 
-// Get the Movie ID from the command-line argument
-const movieId = process.argv[2];
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
+# result list of lists
+res = []
 
-// Construct the URL for the Star Wars API
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+# the locations that will constraint the position of a placed Queen
+col = []
+rightdiag = []
+leftdiag = []
 
-// Fetch the movie data based on the Movie ID
-request(url, (err, response, body) => {
-  if (err) {
-    console.error(err);
-  } else if (response.statusCode === 200) {
-    const movieData = JSON.parse(body); // Parse the movie data
-    const characters = movieData.characters; // Get the list of character URLs
-    fetchCharacters(characters, 0); // Start fetching characters in order
-  } else {
-    console.error('Failed to retrieve movie, status code:', response.statusCode);
-  }
-});
+
+def backtrack(row):
+    """ This recursive function places a Queen starting at row 0
+    then checks for constraints found in lists (col, rightdiag, leftdiag)
+    if found it skips to the next columns ``continue``.
+    """
+    if row >= N:  # basecase for recursion function
+        sol = []
+        for i in range(len(board)):
+            pos = []
+            for column in range(len(board[i])):
+                if board[i][column] == 1:
+                    pos += [[i, column]]
+            sol += pos
+        res.append(sol)  # adding one solution to the result list.
+        return
+
+    for c in range(N):  # check if poisition is restricted by lists.
+        if c in col or (row + c) in rightdiag or (row - c) in leftdiag:
+            continue
+
+        col.append(c)  # code program runs while testing a position.
+        rightdiag.append(row + c)
+        leftdiag.append(row - c)
+        board[row][c] = 1  # places a queen at position.
+
+        backtrack(row + 1)  # recursive call
+
+        col.remove(c)  # code program runs while backtracking.
+        rightdiag.remove(row + c)
+        leftdiag.remove(row - c)
+        board[row][c] = 0
+    return
+
+
+backtrack(0)
+for solution in res:
+    print(solution)
--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,49 @@
 #!/usr/bin/node
+// Prints all characters of a Star Wars movie.
 
-function order (characters, idx) {
-    if (idx >= characters.length) {
-      return;
-    }
-    request(characters[idx], function (err, response, body) {
-      if (err) {
-        console.log(err);
-      } else if (response.statusCode === 200) {
-        const person = JSON.parse(body);
-        console.log(person.name);
-        return order(characters, ++idx);
-      } else {
-        console.log('error ocurred, Status code: ' + response.statusCode);
-      }
-    });
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+request(url, async (err, response, body) => {
+  if (err) {
+    console.error('Error fetching movie:', err);
+    return;
   }
-  
-  const request = require('request');
-  const url = 'https://swapi-api.hbtn.io/api/films/';
-  const ep = process.argv[2];
-  request(url + ep, function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else if (response.statusCode === 200) {
-      const jsobj = JSON.parse(body);
-      order(jsobj.characters, 0);
-    } else {
-      console.log('error ocurred, Status code: ' + response.statusCode);
-    }
-  });
-  
+
+  if (response.statusCode !== 200) {
+    console.error(`Error: Received status code ${response.statusCode} when fetching movie data`);
+    return;
+  }
+
+  const charArr = JSON.parse(body).characters;
+
+  try {
+    const characterPromises = charArr.map(charUrl => {
+      return new Promise((resolve, reject) => {
+        request(charUrl, (err, response, body) => {
+          if (err) {
+            reject('Error fetching character:', err);
+          } else if (response.statusCode !== 200) {
+            reject(`Error: Received status code ${response.statusCode} when fetching character data`);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
+    });
+
+    // Wait for all character names to be fetched
+    const characters = await Promise.all(characterPromises);
+
+    // Print each character name
+    characters.forEach(character => console.log(character));
+  } catch (error) {
+    console.error('Error:', error);
+  }
+});
