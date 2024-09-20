ee52d8fdbc8aa670dde635d4ca58f8029688b1d0
sambutracy <tracysambut@gmail.com>
2024-09-20 04:42:25 +0300

    fix error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import sys
+
 """
 This module provides a function to determine the minimum number of coins
 needed to make a given total amount.
+
 Functions:
     makeChange(coins, total):
         Determines the minimum number of coins needed to make the given total.
+
 Usage:
     Run the script with the following command:
     python3 0-making_change.py <total> <coin1,coin2,...>
+
     Example:
         python3 0-making_change.py 11 1,2,5
 """
@@ -29,3 +33,24 @@ def makeChange(coins, total):
 
     # If dp[total] is still infinity, return -1 (means total can't be met)
     return dp[total] if dp[total] != float('inf') else -1
+
+
+# Command-line interface
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 3:
+        print("Usage: python3 0-making_change.py <total> <coin1,coin2,...>")
+        sys.exit(1)
+
+    try:
+        # Parse the arguments
+        total = int(sys.argv[1])
+        coins = list(map(int, sys.argv[2].split(',')))
+
+        # Call the makeChange function
+        result = makeChange(coins, total)
+        print(result)
+
+    except ValueError:
+        print("Invalid input, integers for the total and coin denominations.")
+        sys.exit(1)
